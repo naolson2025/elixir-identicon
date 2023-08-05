e9c97b0bf3e73de06cc277c29651a44c8e522d3c
Nick Olson <naolson.1819@gmail.com>
2023-08-05 15:09:35 -0500

    finished project

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,23 +1,88 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({top_left, bottom_right}) ->
+      :egd.filledRectangle(image, top_left, bottom_right, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      # rem is a built in fn for remainder
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3,:discard)
+      # The & syntax is to pass a reference to the mirror_row function
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail] = row
+    # append the extracted first & second elements to the end of the list
+    # this will result in a mirrored row
+    row ++ [second, first]
+    # [145, 46, 200, 46, 145] returned mirrored
+  end
+
+  # image is a struct with a list of hex values
+  # use pattern matching to extract the first three elements
+  # from the list
+  # then use the pipe and _tail to ignore the rest
+  # we can pattern match on the received struct
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    # Create a new image struct with all properties
+    # of the above image struct, but add in the color property
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
